0bd099fa05e3521cc2fcd64ffd87ca6d8f8806ff
Newmanben <Newmanbernard761@gmail.com>
2023-03-03 15:31:30 -0800

    _putchar

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,8 +1,7 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * _putchar - Writes the character c to stdout
+ *  _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
@@ -10,5 +9,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+  return (write(1, &c, 1));
 }
